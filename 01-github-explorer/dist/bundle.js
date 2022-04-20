82f1ec5fdf2d67edff1ef4434f4e76b772571952
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-04-20 03:28:55 -0300

    removing node_modules from repo

--- 01-github-explorer/dist/bundle.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-var _App = require("./App");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
