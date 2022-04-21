71ebd482ef8feaea4f25459348d967d4bf45f310
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-04-21 00:12:13 -0300

    react version 18 correct sintax

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,3 +1,8 @@
 export function App() {
-  return <p>You are in the APP.jsx</p>;
+  return (
+    <p>
+      You are in the APP.jsx!, now the webpack-dev-server is on and the
+      application is with the live-reloading enabled!
+    </p>
+  );
 }
