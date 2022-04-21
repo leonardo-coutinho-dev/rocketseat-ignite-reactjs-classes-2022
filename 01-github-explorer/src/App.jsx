a771cb2c43516e040a54b636fe4dc6d7a1223c43
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-04-21 03:23:22 -0300

    configuring webpack dev serve and webpack source map

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,8 +1,12 @@
 export function App() {
+  throw new Error('Eita Giovana, o forninho caiu!');
   return (
-    <p>
-      You are in the APP.jsx!, now the webpack-dev-server is on and the
-      application is with the live-reloading enabled!
-    </p>
+    <>
+      <h1>Texto 1</h1>
+      <h2>Texto 2</h2>
+      <h3>Texto 3</h3>
+      <h4>Texto 4</h4>
+      <h5>Texto 5</h5>
+    </>
   );
 }
