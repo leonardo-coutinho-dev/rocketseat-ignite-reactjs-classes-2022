322c40c857c1f17ef818a7bd7721cf49e6fb1c1b
leonardo <web.dev.trabalho@gmail.com>
2022-09-15 00:39:42 -0300

    adding styles to modal content

--- 02-dt-money/src/components/NewTransactionModal/index.tsx
+++ 02-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import Modal from "react-modal";
 
 import closeSvg from "../../assets/close.svg";
+import incomeSvg from "../../assets/income.svg";
+import outcomeSvg from "../../assets/outcome.svg";
 
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -31,6 +33,16 @@ export const NewTransactionModal = ({
         <h2>Cadastrar transação</h2>
         <input placeholder="Título" type="text" />
         <input placeholder="Valor" type="number" />
+        <TransactionTypeContainer>
+          <button>
+            <img src={incomeSvg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button>
+            <img src={outcomeSvg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
         <input placeholder="Categoria" type="text" />
         <button type="submit">Cadastrar</button>
       </Container>
